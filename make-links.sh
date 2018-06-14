cb7cfbc7e2fb97cbea591134968d97664638e21a
Dmitry Olefir <dmitry.commits@gmail.com>
2018-06-14 00:32:06 +0000

    Add new file

---
+++ make-links.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cd `dirname $0`
+CONF_DIR=$(pwd)
+
+make_links () {
+    ln -sf $CONF_DIR/.conkyrc $1/
+    ln -sf $CONF_DIR/.gtkrc-2.0 $1/
+    ln -sf $CONF_DIR/.xinitrc $1/
+    ln -sf $CONF_DIR/.xprofile $1/
+    ln -sf $CONF_DIR/.Xresources $1/
+    ln -sf $CONF_DIR/.xserverrc $1/
+    
+    ln -sf $CONF_DIR/x $1/
+    mkdir -p $1/scripts/
+    ln -sf $CONF_DIR/x/x-desktop.sh $1/scripts/
+    
+    mkdir -p $1/.config/
+    ln -sf $CONF_DIR/.config/dunst $1/.config/
+    ln -sf $CONF_DIR/.config/gtk-3.0 $1/.config/
+    ln -sf $CONF_DIR/.config/i3 $1/.config/
+    ln -sf $CONF_DIR/.config/i3status $1/.config
+    ln -sf $CONF_DIR/.config/termite $1/.config
+}
+
+# Make links in our home directory
+make_links ~
+
+# Make links in root home directory
+MAKE_LINKS_FUNC=$(declare -f make_links)
+sudo bash -c "CONF_DIR=$CONF_DIR;$MAKE_LINKS_FUNC; make_links /root"
