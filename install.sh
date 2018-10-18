db3061a8bce22ff9ada21c6a44e1578184551b5e
ivanp7 <ivanp7.git@gmail.com>
2018-10-18 16:30:46 +0300

    Add instruction to set correct owner and permissions on .conkyrc

--- install.sh
+++ install.sh
@@ -11,6 +11,8 @@ install_links () {
     ln -sf $CONF_DIR/.xserverrc $HOME/
     
     cp $CONF_DIR/.conkyrc_template $HOME/.conkyrc
+    chown $(whoami): $HOME/.conkyrc
+    chmod 644 $HOME/.conkyrc
     sed -i "s/NETWORK_INTERFACE/$(grep 'Interface=' /etc/netctl/network | cut -d'=' -f2)/g" $HOME/.conkyrc
 
     ln -sf $CONF_DIR/.gtkrc-2.0 $HOME/
