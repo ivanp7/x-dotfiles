6948423b591c22636cc94b56afecb8ed81db18b6
ivanp7 <ivanp7.git@gmail.com>
2018-10-21 21:12:45 +0300

    Disable creation of ~/.fehbg

--- .config/i3/wallpapers.sh
+++ .config/i3/wallpapers.sh
@@ -3,7 +3,7 @@
 xprop -spy -root _NET_CURRENT_DESKTOP | while read -r event; do
     num=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true) | .num')
     wallpaper=$HOME/wallpapers/${num}.wallpaper
-    if [[ -f $wallpaper ]]; then feh --bg-scale $wallpaper;
-    else feh --bg-scale $HOME/wallpapers/default.wallpaper; fi
+    if [[ -f $wallpaper ]]; then feh --no-fehbg --bg-scale $wallpaper;
+    else feh --no-fehbg --bg-scale $HOME/wallpapers/default.wallpaper; fi
 done
 
