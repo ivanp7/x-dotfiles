199681b5aa22135803efb8187a11c07976b559f2
ivanp7 <ivanp7.git@gmail.com>
2018-10-22 18:08:22 +0300

    Fix wait-and-move.sh

--- .config/i3/bin/wait-and-move.sh
+++ .config/i3/bin/wait-and-move.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-pids="($(sed 's/ /|/g' <<< $1))"
-
 while true
 do
-    sleep 0.1
+    sleep 0.5
+    pids="($(sed 's/ /|/g' <<< "`pidof $1`"))"
     expected=$2
     actual=`wmctrl -lp | awk '{print $3}' | egrep -o "$pids" | wc -l`
     [[ $expected -gt $actual ]] || break
 done
 
-windowIds=`wmctrl -lp | awk '{print $1" "$3}' | sed -e 's/\(0x[0-9a-f]\{8\}\) '$1'/\1/;tx;d;:x'`
+windowIds=`wmctrl -lp | egrep "$pids" | awk '{print $1}'`
 
 echo "$windowIds" | xargs -i i3-msg '[id="{}"] move container to workspace '$3
 
