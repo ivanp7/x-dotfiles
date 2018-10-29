97af12f7e7f0e3201ae4bb20c8334ae88adcc5e5
ivanp7 <ivanp7.git@gmail.com>
2018-10-29 16:57:53 +0300

    Perform calendar updating task using cron

--- .config/i3/layout/update_calendar.sh
+++ .config/i3/layout/update_calendar.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-TASK="sh $HOME/.config/i3/layout/update_calendar_midnight.sh"
-for job in `atq | awk '{print $1}'`
-do
-    at -c $job | fgrep $TASK > /dev/null && TASK_EXISTS=yes; break
-done
-[[ -z $TASK_EXISTS ]] && echo $TASK | at midnight > /dev/null 2>&1
-
-##################################################################
-
 when ci
 while true
 do
--- .config/i3/layout/update_calendar_midnight.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-{ type xhost >& /dev/null && xhost >& /dev/null; } || exit
-
-echo -n "" >> $HOME/Org/calendar
-echo "sh $0" | at midnight > /dev/null 2>&1
-
