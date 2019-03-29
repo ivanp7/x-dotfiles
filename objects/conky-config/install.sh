f15fa174443e13e272aec2485c0c2d8e2b220f2c
ivanp7 <ivanp7.git@gmail.com>
2019-03-29 20:47:30 +0300

    Delete conky and i3status configuration files

--- objects/conky-config/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CONF_DIR=$(realpath `dirname $0`)
-
-install -Dm 644 $CONF_DIR/.conkyrc $HOME/
-sed -i "s/NETWORK_INTERFACE/$(grep 'Interface=' /etc/netctl/network | cut -d'=' -f2)/g" $HOME/.conkyrc
-
