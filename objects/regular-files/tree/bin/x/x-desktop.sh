87edd42d058a88064f6d02dddbd29a5ed1242cd7
ivanp7 <ivanp7.git@gmail.com>
2019-04-04 22:51:15 +0300

    Start X from the home directory only

--- objects/regular-files/tree/bin/x/x-desktop.sh
+++ objects/regular-files/tree/bin/x/x-desktop.sh
@@ -5,6 +5,7 @@ mkdir -p /tmp/tmux-refresh-service-$(whoami)
 TTY=$(tty | sed 's@/dev/@@; s@/@@')
 touch /tmp/tmux-refresh-service-$(whoami)/$TTY
 
+cd $HOME
 startx
 
 rm /tmp/tmux-refresh-service-$(whoami)/$TTY
