a99109942abd0bd741803ff43e55a7b77d5fa5a6
ivanp7 <ivanp7.git@gmail.com>
2019-04-10 20:07:22 +0300

    Improve polybar typing speed indicator

--- objects/regular-files/tree/.config/polybar/info-hackspeed.sh
+++ objects/regular-files/tree/.config/polybar/info-hackspeed.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # shellcheck disable=SC2016,SC2059
 
-KEYBOARD_ID="AT Translated Set 2 keyboard"
+KEYBOARD_ID="$(cat $(dirname $0)/kbd_name)"
 
 # cpm: characters per minute
 # wpm: words per minute (1 word = 5 characters)
