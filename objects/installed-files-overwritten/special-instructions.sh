c31677db99d7bd47dbcaf120f03a8bab4e9db2f1
ivanp7 <ivanp7.git@gmail.com>
2019-04-21 00:38:24 +0300

    Add minor fix to network interfaces name detection commands

--- objects/installed-files-overwritten/special-instructions.sh
+++ objects/installed-files-overwritten/special-instructions.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sed -i "s/WIRED_NETWORK_INTERFACE/$(ip link | egrep "^[[:digit:]]:" | cut -d' ' -f2 | tr -d : | egrep "^en" | head -n1)/g" $HOME/.config/polybar/config
-sed -i "s/WIRELESS_NETWORK_INTERFACE/$(ip link | egrep "^[[:digit:]]:" | cut -d' ' -f2 | tr -d : | egrep "^wl" | head -n1)/g" $HOME/.config/polybar/config
+sed -i "s/WIRED_NETWORK_INTERFACE/$(ip link | egrep "^[[:digit:]]*:" | cut -d' ' -f2 | tr -d : | egrep "^en" | head -n1)/g" $HOME/.config/polybar/config
+sed -i "s/WIRELESS_NETWORK_INTERFACE/$(ip link | egrep "^[[:digit:]]*:" | cut -d' ' -f2 | tr -d : | egrep "^wl" | head -n1)/g" $HOME/.config/polybar/config
 
