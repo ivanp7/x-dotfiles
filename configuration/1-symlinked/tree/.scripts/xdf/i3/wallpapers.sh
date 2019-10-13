7ab294dda11f0e29f1bee026777567512c16c406
ivanp7 <ivanp7.git@gmail.com>
2019-10-13 17:30:37 +0300

    Get rid of xprop use in wallpapers.sh as it doesn't work properly anymore

--- configuration/1-symlinked/tree/.scripts/xdf/i3/wallpapers.sh
+++ configuration/1-symlinked/tree/.scripts/xdf/i3/wallpapers.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-xprop -spy -root _NET_CURRENT_DESKTOP | while read -r event; do
+num_prev=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true) | .num')
+
+# xprop -spy -root _NET_CURRENT_DESKTOP | while read -r event; do
+while true
+do
+    sleep 0.25
     num=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true) | .num')
-    wallpaper=$HOME/wallpapers/${num}.wallpaper
-    if [ -f $wallpaper ]; then feh --no-fehbg --bg-scale $wallpaper;
-    else feh --no-fehbg --bg-scale $HOME/wallpapers/default.wallpaper; fi
+
+    if [ "$num" -ne "$num_prev" ]
+    then
+        num_prev=$num
+        wallpaper=$HOME/wallpapers/${num}.wallpaper
+        if [ -f $wallpaper ]; then feh --no-fehbg --bg-scale $wallpaper;
+        else feh --no-fehbg --bg-scale $HOME/wallpapers/default.wallpaper; fi
+    fi
 done
 
