b6d74c4ebeb6402c4c61f0248a33a8f33ea2726c
ivanp7 <ivanp7.git@gmail.com>
2019-11-27 09:57:11 +0300

    Switch back to st terminal

--- configuration/1-symlinked/tree/bin/xdf/dropdown-terminal.sh
+++ configuration/1-symlinked/tree/bin/xdf/dropdown-terminal.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if ! pgrep -f "alacritty --class dropdown_terminal"
+if ! pgrep -f "st -c dropdown_terminal"
 then
-    i3-msg 'exec --no-startup-id alacritty --class dropdown_terminal -t "Dropdown terminal"'
+    i3-msg 'exec --no-startup-id st -c dropdown_terminal -t "Dropdown terminal"'
 else
     i3-msg '[class="dropdown_terminal"] scratchpad show'
     i3-msg '[instance="dropdown_terminal"] scratchpad show'
