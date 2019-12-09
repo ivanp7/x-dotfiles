a554fbf39754eb263f00c55abf9c708904fdd394
ivanp7 <ivanp7.git@gmail.com>
2019-12-09 10:19:48 +0300

    Add dwm status bar script

---
+++ configuration/1-symlinked/tree/.scripts/xdf/dwm_status.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+while sleep 0.25
+do
+    VOLUME=$(volume.sh)
+    LANGUAGE=$(case $(xset -q | grep LED | sed -E 's/.*LED mask: *....(.).../\1/') in 0) echo English ;; 1) echo Russian ;; esac)
+    DATETIME=$(date +'%Y-%b-%d %a %H:%M:%S')
+    xsetroot -name "$VOLUME | $LANGUAGE | $DATETIME"
+done
+
