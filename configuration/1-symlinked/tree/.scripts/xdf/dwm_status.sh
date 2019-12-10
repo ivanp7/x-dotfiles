6d61046f8adfb7702cc25effeccb19775926295b
ivanp7 <ivanp7.git@gmail.com>
2019-12-10 14:44:24 +0300

    Add internet connection status indicator to dwm bar

--- configuration/1-symlinked/tree/.scripts/xdf/dwm_status.sh
+++ configuration/1-symlinked/tree/.scripts/xdf/dwm_status.sh
@@ -1,12 +1,51 @@
 #!/bin/sh
 
+INTERVAL=0.25
 SEPARATOR="|"
 
-while sleep 0.25
+###############################################################################
+
+PING_INTERVAL=1
+PING_RESULT="pinging"
+PING_RESULT_FILE=$(mktemp)
+ping_internet ()
+{
+    if ping -c 1 -w $PING_INTERVAL "archlinux.org" > /dev/null 2>&1
+    then echo -n "online!" > $PING_RESULT_FILE
+    else echo -n "offline" > $PING_RESULT_FILE
+    fi
+}
+
+###############################################################################
+
+language ()
+{
+    case $(xset -q | grep LED | sed -E 's/.*LED mask: *....(.).../\1/') in 
+        0) echo English ;;
+        1) echo Russian ;;
+    esac
+}
+
+###############################################################################
+
+datetime ()
+{
+    date +"%Y-%b-%d %a $SEPARATOR %H:%M:%S"
+}
+
+###############################################################################
+
+while sleep $INTERVAL
 do
-    VOLUME=$(volume.sh)
-    LANGUAGE=$(case $(xset -q | grep LED | sed -E 's/.*LED mask: *....(.).../\1/') in 0) echo English ;; 1) echo Russian ;; esac)
-    DATETIME=$(date +"%Y-%b-%d %a $SEPARATOR %H:%M:%S")
-    xsetroot -name "$VOLUME $SEPARATOR $LANGUAGE $SEPARATOR $DATETIME"
+    if [ -f "$PING_RESULT_FILE" ]
+    then
+        PING_RESULT="$(cat $PING_RESULT_FILE)"
+        rm $PING_RESULT_FILE
+        ping_internet &
+    fi
+
+    xsetroot -name "$PING_RESULT $SEPARATOR $(volume.sh) $SEPARATOR $(language) $SEPARATOR $(datetime)"
 done
+
+rm "$PING_RESULT_FILE"
 
