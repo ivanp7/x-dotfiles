0d30e8d7a0c2551ee6dc72f8104c5bce0635cc68
ivanp7 <ivanp7.git@gmail.com>
2019-12-25 00:17:23 +0300

    Break long lines in polybar.sh

--- configuration/9-special/instructions/polybar.sh
+++ configuration/9-special/instructions/polybar.sh
@@ -3,12 +3,15 @@
 case $1 in
     i)
         if [ -r "$HOME/.default_font" ]
-        then cat $HOME/.default_font | xargs -I {} sed -i 's/DEFAULT_FONT/{}/g' $HOME/.config/polybar/config
+        then cat $HOME/.default_font | 
+            xargs -I {} sed -i 's/DEFAULT_FONT/{}/g' $HOME/.config/polybar/config
         else sed -i 's/DEFAULT_FONT/'"$DEFAULT_FONT"'/g' $HOME/.config/polybar/config
         fi
 
-        ip link | grep -E '^[[:digit:]]*: *en.*:' | sed -E 's/^(.*): *(.*): (.*)/\2/' | head -n1 | xargs -I {} sed -i 's/WIRED_NETWORK_INTERFACE/{}/g' $HOME/.config/polybar/config
-        ip link | grep -E '^[[:digit:]]*: *wl.*:' | sed -E 's/^(.*): *(.*): (.*)/\2/' | head -n1 | xargs -I {} sed -i 's/WIRELESS_NETWORK_INTERFACE/{}/g' $HOME/.config/polybar/config
+        ip link | grep -E '^[[:digit:]]*: *en.*:' | sed -E 's/^(.*): *(.*): (.*)/\2/' | head -n1 | 
+            xargs -I {} sed -i 's/WIRED_NETWORK_INTERFACE/{}/g' $HOME/.config/polybar/config
+        ip link | grep -E '^[[:digit:]]*: *wl.*:' | sed -E 's/^(.*): *(.*): (.*)/\2/' | head -n1 | 
+            xargs -I {} sed -i 's/WIRELESS_NETWORK_INTERFACE/{}/g' $HOME/.config/polybar/config
         ;;
 
     u)
