451693a3309d7ab88a762088a52fd4dd188a62e5
ivanp7 <ivanp7.git@gmail.com>
2019-12-26 00:38:41 +0300

    Ignore comments and empty lines in surf bookmarks file

--- configuration/1-symlinked/tree/.scripts/xdf/surf/surf_bookmarks_dmenu.sh
+++ configuration/1-symlinked/tree/.scripts/xdf/surf/surf_bookmarks_dmenu.sh
@@ -3,6 +3,6 @@
 SURF_WINDOW="${1:-$(xprop -root | sed -n '/^_NET_ACTIVE_WINDOW/ s/.* //p')}"
 DMENU_PROMPT="${2:-Bookmark}"
 
-tac "$HOME/.surf/bookmarks.txt" | 
+tac "$HOME/.surf/bookmarks.txt" | sed -E 's/^#.*//; /^\s*$/d' | 
     dmenu.sh -p "$DMENU_PROMPT" -l 10 -i -w $SURF_WINDOW | cut -d' ' -f1
 
