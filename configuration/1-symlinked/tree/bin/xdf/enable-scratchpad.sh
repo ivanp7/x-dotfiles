c55fd1f1183d782076e117e48914e2feeedbcbf9
ivanp7 <ivanp7.git@gmail.com>
2019-12-26 10:52:22 +0300

    Improve bspwm behaviour on scratchpad and surf windows

--- configuration/1-symlinked/tree/bin/xdf/enable-scratchpad.sh
+++ configuration/1-symlinked/tree/bin/xdf/enable-scratchpad.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+ps ax -o pid,args | grep "$TERMINAL -c scratchpad" | grep -v "grep" | 
+    awk '{print $1}' | xargs -r kill
 $TERMINAL -c scratchpad -t scratchpad &
 sleep 1
 bspc query -N -n .floating > /tmp/scratchid
