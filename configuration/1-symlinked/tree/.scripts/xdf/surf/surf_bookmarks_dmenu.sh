f16e467d0e76cd3796911f2c049a75f3b0915cf3
ivanp7 <ivanp7.git@gmail.com>
2019-12-27 19:23:24 +0300

    Implement hierachical bookmarking in surf

--- configuration/1-symlinked/tree/.scripts/xdf/surf/surf_bookmarks_dmenu.sh
+++ configuration/1-symlinked/tree/.scripts/xdf/surf/surf_bookmarks_dmenu.sh
@@ -3,6 +3,7 @@
 SURF_WINDOW="${1:-$(xprop -root | sed -n '/^_NET_ACTIVE_WINDOW/ s/.* //p')}"
 DMENU_PROMPT="${2:-Bookmark}"
 
-tac "$HOME/.surf/bookmarks.txt" | sed -E 's/^#.*//; /^\s*$/d' | 
+cat "$HOME/.surf/bookmarks.txt" $(find "$HOME/.surf/bookmarks/" -type f) | 
+    sed -E 's/\s*".*//; /^\s*$/d' | sed -E 's/^([^ ]*) *(.*)/\2 <\1>/' |
     dmenu.sh -p "$DMENU_PROMPT" -l 10 -i -w $SURF_WINDOW | cut -d' ' -f1
 
