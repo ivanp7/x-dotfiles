94f2b846aaef58e2eb7c9402a0041696adc2a556
ivanp7 <ivanp7.git@gmail.com>
2020-01-09 23:41:34 +0300

    Improve polybar and keyboard layout notifier configuration

--- 1-symlinked/tree/.scripts/xdf/keyboard-layout-notifications.sh
+++ 1-symlinked/tree/.scripts/xdf/keyboard-layout-notifications.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
+prev_layout="English"
 while xhost > /dev/null 2>&1
 do 
     polybar -s keyboard_layout_only | 
         stdbuf -oL sed -E 's/ ?%\{.}$//; s/%\{.*\}//' | 
         while read layout
         do 
-            if echo $layout | grep -q '^English.*'
-            then urgency=normal
-            else urgency=critical
+            if [ "$layout" != "$prev_layout" ]
+            then
+                if echo $layout | grep -q '^English.*'
+                then urgency=normal
+                else urgency=critical
+                fi
+                notify-send -t 500 -u $urgency "$layout"
+                prev_layout=$layout
             fi
-            notify-send -t 500 -u $urgency "$layout"
         done
 done
 
--- 1-symlinked/tree/bin/xdf/desktops.sh
+++ 1-symlinked/tree/bin/xdf/desktops.sh
@@ -2,10 +2,26 @@
 
 N=$(xdotool get_num_desktops)
 
+to_name ()
+{
+    cat | sed '
+s/10/X/;
+s/1/I/;
+s/2/II/;
+s/3/III/;
+s/4/IV/;
+s/5/V/;
+s/6/VI/;
+s/7/VII/;
+s/8/VIII/;
+s/9/IX/;
+    '
+}
+
 case $1 in
     "") echo $N ;;
-    inc) [ "$N" -lt 10 ] && bspc monitor -d $(seq $(($N + 1))) ;;
-    dec) [ "$N" -gt 1 ] && bspc monitor -d $(seq $(($N - 1))) ;;
-    *) bspc monitor -d $(seq $1) ;;
+    inc) [ "$N" -lt 10 ] && bspc monitor -d $(seq $(($N + 1)) | to_name) ;;
+    dec) [ "$N" -gt 1 ] && bspc monitor -d $(seq $(($N - 1)) | to_name) ;;
+    *) bspc monitor -d $(seq $1 | to_name) ;;
 esac
 
