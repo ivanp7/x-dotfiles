7bed2e6535eecbb2847ad750a0ba0e29c3e23b12
ivanp7 <ivanp7.git@gmail.com>
2020-01-11 00:43:16 +0300

    Implement selection of category during bookmark selection

--- 1-symlinked/tree/.scripts/xdf/surf/surf_bookmarks_dmenu.sh
+++ 1-symlinked/tree/.scripts/xdf/surf/surf_bookmarks_dmenu.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-SURF_WINDOW="${1:-$(xprop -root | sed -n '/^_NET_ACTIVE_WINDOW/ s/.* //p')}"
-DMENU_PROMPT="${2:-Bookmark}"
+if [ -n "$1" ]
+then WINDOW_ARG="-w $1"; fi
 
-cat "$HOME/.surf/bookmarks.txt" $(find "$HOME/.surf/bookmarks/" -type f) | 
+DMENU_PROMPT_PATHS="Category"
+DMENU_PROMPT_URLS="Bookmark"
+DMENU_LINES=10
+
+cd $HOME/.bookmarks
+
+find . | dmenu.sh -p "$DMENU_PROMPT_PATHS" -l $DMENU_LINES -i $WINDOW_ARG | 
+    xargs -I {} find {} -type f | xargs -I {} cat {} | 
     sed -E 's/\s*".*//; /^\s*$/d' | sed -E 's/^([^ ]*) *(.*)/\2 <\1>/' |
-    dmenu.sh -p "$DMENU_PROMPT" -l 10 -i -w $SURF_WINDOW | 
+    ifne dmenu.sh -p "$DMENU_PROMPT_URLS" -l $DMENU_LINES -i $WINDOW_ARG | 
     sed -E 's/.*<([^ ]*)>$/\1/'
 
--- 1-symlinked/tree/bin/xdf/bookmarks.sh
+++ 1-symlinked/tree/bin/xdf/bookmarks.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-DMENU_PROMPT="${1:-Bookmark}"
-
-cat $(find "$HOME/.bookmarks/" -type f) | 
-    sed -E 's/\s*".*//; /^\s*$/d' | sed -E 's/^([^ ]*) *(.*)/\2 <\1>/' |
-    dmenu.sh -p "$DMENU_PROMPT" -l 10 -i | sed -E 's/.*<([^ ]*)>$/\1/' | 
-    xclip -i -selection clipboard
+$HOME/.scripts/xdf/surf/surf_bookmarks_dmenu.sh | xclip -i -selection clipboard
 
