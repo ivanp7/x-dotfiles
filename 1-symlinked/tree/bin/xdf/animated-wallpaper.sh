d420e60d2b5ca354eb262577de199be61c822fec
ivanp7 <ivanp7.git@gmail.com>
2020-01-17 22:16:24 +0300

    Improve image and animation selection commands

--- 1-symlinked/tree/bin/xdf/animated-wallpaper.sh
+++ 1-symlinked/tree/bin/xdf/animated-wallpaper.sh
@@ -2,18 +2,30 @@
 
 start_wallpaper ()
 {
-    MPV_OPTIONS="--osc=no --osd-level=0 --no-input-default-bindings --vo=vdpau --hwdec=vdpau --loop=inf --start=$(($2 % 100))%"
+    if [ -z "$2" ]
+    then RAND=$(shuf -i 0-99 -n 1)
+    else RAND=$(($2 % 100))
+    fi
+    MPV_OPTIONS="--osc=no --osd-level=0 --no-input-default-bindings --vo=vdpau --hwdec=vdpau --loop=inf --start=$RAND%"
 
     # dependency: shantz-xwinwrap-bzr from AUR
-    [ -n "$1" ] && [ -f "$1" ] && \
-        exec xwinwrap -ov -fs -- mpv -wid WID $MPV_OPTIONS "$1" > /dev/null 2>&1 &
+    if [ -f "$1" ]
+    then exec xwinwrap -ov -fs -- mpv -wid WID $MPV_OPTIONS "$1" > /dev/null 2>&1 &
+    fi
 }
 
 # kill animated wallpaper, if it is displayed
 pkill xwinwrap
 
 # display random video
-WALLPAPER="$(find -L $HOME/wallpapers/ -name "*.ani" | shuf -n1)"
-if [ -z "$1" ]; then RAND=0; else RAND=$1; fi
-start_wallpaper "$WALLPAPER" "$RAND"
+WALLPAPER="$(find -L $HOME/wallpapers/ -type f -o -type l | 
+    while read file
+    do
+        if xdg-mime query filetype "$file" | grep -q "^video/"
+        then echo "$file"
+        fi
+    done | shuf -n1)"
+[ -z "$WALLPAPER" ] && exit 1
+
+start_wallpaper "$WALLPAPER" $1
 
--- 1-symlinked/tree/bin/xdf/classic-wallpaper.sh
+++ 1-symlinked/tree/bin/xdf/classic-wallpaper.sh
@@ -4,6 +4,14 @@
 pkill xwinwrap
 
 # display random picture
-WALLPAPER="$(find -L $HOME/wallpapers/ -name "*.pic" | shuf -n1)"
-[ -n "$WALLPAPER" ] && feh --no-fehbg --bg-scale "$WALLPAPER"
+WALLPAPER="$(find -L $HOME/wallpapers/ -type f -o -type l | 
+    while read file
+    do
+        if xdg-mime query filetype "$file" | grep -q "^image/"
+        then echo "$file"
+        fi
+    done | shuf -n1)"
+[ -z "$WALLPAPER" ] && exit 1
+
+feh --no-fehbg --bg-scale "$WALLPAPER"
 
