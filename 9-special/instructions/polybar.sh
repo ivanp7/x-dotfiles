aecb6644f302089450ce16079a054a4bc1960e11
ivanp7 <ivanp7.git@gmail.com>
2020-02-27 12:09:21 +0300

    Add fallback font for polybar configuration

--- 9-special/instructions/polybar.sh
+++ 9-special/instructions/polybar.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ -z "$DEFAULT_FONT" ]
+then DEFAULT_FONT="xos4 Terminus:size=10"
+fi
+
 sed -i 's/DEFAULT_FONT/'"$DEFAULT_FONT"'/g' $HOME/.config/polybar/config
 
 ip link | grep -E '^[[:digit:]]*: *en.*:' | sed -E 's/^(.*): *(.*): (.*)/\2/' | head -n1 | 
