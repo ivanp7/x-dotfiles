9638fddc37c75b950168149d479c2649bd8185f4
ivanp7 <ivanp7.git@gmail.com>
2020-05-19 13:56:00 +0300

    Add passwords copying dmenu script

--- 1-symlinked/tree/bin/xdf/pass.sh
+++ 1-symlinked/tree/bin/xdf/pass.sh
@@ -6,9 +6,11 @@ DMENU_PROMPT=Password
 DMENU_LINES=10
 
 cd $PASSWORD_STORE
-find . -path "./.git" -prune -o -type f \! -name ".gitignore" \
-    \! -name ".gitattributes" \! -name ".gpg-id" -print |
+
+export PINENTRY_USER_DATA=X
+find . -path "./.git" -prune -o -type f -name "*.gpg" -print |
     sed 's@^\./@@; s@\.gpg@@' |
     dmenu -fn "$DEFAULT_FONT" -p "$DMENU_PROMPT" -l $DMENU_LINES | 
-    xclip -i -sel clip
+    xargs -r pass -c
+export PINENTRY_USER_DATA=
 
