ef1c21cf69e0695c8649991733bda57da12048d6
ivanp7 <ivanp7.git@gmail.com>
2020-05-19 13:59:25 +0300

    Add cosmetic fix to pass.sh

--- 1-symlinked/tree/bin/xdf/pass.sh
+++ 1-symlinked/tree/bin/xdf/pass.sh
@@ -12,5 +12,4 @@ find . -path "./.git" -prune -o -type f -name "*.gpg" -print |
     sed 's@^\./@@; s@\.gpg@@' |
     dmenu -fn "$DEFAULT_FONT" -p "$DMENU_PROMPT" -l $DMENU_LINES | 
     xargs -r pass -c
-export PINENTRY_USER_DATA=
 
