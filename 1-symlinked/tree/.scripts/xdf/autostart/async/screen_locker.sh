720ed213a03410f29d79ae190bf77141f52003ee
ivanp7 <ivanp7.git@gmail.com>
2020-06-15 12:27:54 +0300

    Improve screen locker script

--- 1-symlinked/tree/.scripts/xdf/autostart/async/screen_locker.sh
+++ 1-symlinked/tree/.scripts/xdf/autostart/async/screen_locker.sh
@@ -4,12 +4,10 @@ CHECK_INTERVAL=60
 
 while xhost > /dev/null 2>&1
 do
-    LOCK_ENABLED=""; [ -f "/tmp/no-screen-lock_$USER" ] || LOCK_ENABLED="yes"
-
-    if [ -n "$LOCK_ENABLED" ] && [ "$(xssstate -t)" -eq 0 ]
+    if [ "$(lock-screen.sh -)" = "enabled" ] && [ "$(xssstate -t)" -eq 0 ]
     then
         date >> /tmp/screen-lock_$USER.log
-        slock
+        lock-screen.sh
     fi
 
     sleep $CHECK_INTERVAL
