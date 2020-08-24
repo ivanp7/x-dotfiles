089e6da09c05556427e6682ac0883938261c2bc8
ivanp7 <ivanp7.git@gmail.com>
2020-08-24 23:05:56 +0300

    Fix sxhkd node cycling mappings; add mapping for pass.sh

--- 1-symlinked/tree/bin/xdf/applications/pass.sh
+++ 1-symlinked/tree/bin/xdf/applications/pass.sh
@@ -2,11 +2,12 @@
 
 DMENU_PROMPT=Password
 DMENU_LINES=10
+DMENU_COLUMNS=3
 
 cd $PASSWORD_STORE_DIR
 
 export PINENTRY_USER_DATA=X
 find . -path "./.git" -prune -o -type f -name "*.gpg" -print |
     sed 's@^\./@@; s@\.gpg@@' |
-    dmenu.sh -p "$DMENU_PROMPT" -l $DMENU_LINES | xargs -r pass -c
+    dmenu.sh -p "$DMENU_PROMPT" -l $DMENU_LINES -g $DMENU_COLUMNS | xargs -r pass -c
 
