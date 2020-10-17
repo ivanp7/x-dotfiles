f1c204d5e108443d11b82fea4a4c5f0556677f69
ivanp7 <ivanp7.git@gmail.com>
2020-10-17 14:06:56 +0300

    Add minor improvements to dmenu invokations

--- 1-symlinked/tree/.scripts/xdf/surf/surf_bookmarks_dmenu.sh
+++ 1-symlinked/tree/.scripts/xdf/surf/surf_bookmarks_dmenu.sh
@@ -5,12 +5,13 @@ DMENU_PROMPT_BOOKMARK="${2:-Bookmark}"
 DMENU_PROMPT_CATEGORY="${3:-Category}"
 
 DMENU_LINES=10
+DMENU_COLUMNS=3
 
 BOOKMARKS_DIR=$XDG_DATA_HOME/bookmarks
 cd "$BOOKMARKS_DIR" || exit 1
 
 find . -path "./.git" -prune -o \! -name ".gitignore" -print | sed 's,^\./,,' | sort | 
-    dmenu -p "$DMENU_PROMPT_CATEGORY" -l $DMENU_LINES -i $WINDOW_ARG | 
+    dmenu.sh -p "$DMENU_PROMPT_CATEGORY" -l $DMENU_LINES -g $DMENU_COLUMNS -i $WINDOW_ARG | 
     xargs -I {} find {} -path "./.git" -prune -o \! -name ".gitignore" -type f -print |
     xargs -I {} cat {} | sed -E 's/\s*".*//; /^\s*$/d' | sed -E 's/^([^ ]*) *(.*)/\2 <\1>/' |
     ifne dmenu -p "$DMENU_PROMPT_BOOKMARK" -l $DMENU_LINES -i $WINDOW_ARG | 
