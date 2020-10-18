5f8e09d2083c3e32a53138754a6cbd83c55f9cb1
ivanp7 <ivanp7.git@gmail.com>
2020-10-18 17:24:06 +0300

    Fix lock-screen.sh

--- 1-symlinked/tree/bin/xdf/graphical-environment/lock-screen.sh
+++ 1-symlinked/tree/bin/xdf/graphical-environment/lock-screen.sh
@@ -5,6 +5,6 @@ case "$1" in
     -on) rm $STATUS_FILE ;;
     -off) touch $STATUS_FILE ;;
     -*) [ -f "$STATUS_FILE" ] && echo disabled || echo enabled ;;
-    *) [ -f "$STATUS_FILE" ] || { xkb-switch -s us; slock & $@; }
+    *) [ -f "$STATUS_FILE" ] || { xkb-switch -s us; slock; } & $@
 esac
 
