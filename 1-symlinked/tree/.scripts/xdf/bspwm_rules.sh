f2307edb1231d1b9ae45dcdeaca84f804afa2f54
ivanp7 <ivanp7.git@gmail.com>
2020-10-20 02:21:20 +0300

    Improve bspwm_rules.sh

--- 1-symlinked/tree/.scripts/xdf/bspwm_rules.sh
+++ 1-symlinked/tree/.scripts/xdf/bspwm_rules.sh
@@ -6,35 +6,47 @@ INSTANCE_NAME=$3
 shift 3
 CONSEQUENCES=$@
 
-#
-# Auxiliary variables
-#
-
 SCREEN_DIMENSIONS=$(xdpyinfo | grep dimensions | awk '{print $2}')
 SCREEN_WIDTH=$(echo "$SCREEN_DIMENSIONS" | cut -d'x' -f1)
 SCREEN_HEIGHT=$(echo "$SCREEN_DIMENSIONS" | cut -d'x' -f2)
 
-FLOATING_WIDTH=$(($SCREEN_WIDTH * 7 / 8))
-FLOATING_HEIGHT=$(($SCREEN_HEIGHT * 7 / 8))
-FLOATING_X=$(( ($SCREEN_WIDTH - $FLOATING_WIDTH) / 2 ))
-FLOATING_Y=$(( ($SCREEN_HEIGHT - $FLOATING_HEIGHT) / 2 ))
-FLOATING_RECTANGLE="$FLOATING_WIDTH"x"$FLOATING_HEIGHT"+"$FLOATING_X"+"$FLOATING_Y"
+central_rectangle ()
+{
+    local width=$1
+    local height=$2
+
+    local x=$((($SCREEN_WIDTH - $width) / 2))
+    local y=$((($SCREEN_HEIGHT - $height) / 2))
+
+    echo "${width}x${height}+${x}+${y}"
+}
+
+#
+# Auxiliary variables
+#
+
+DROPDOWN_WIDTH=$(($SCREEN_WIDTH * 7 / 8))
+DROPDOWN_HEIGHT=$(($SCREEN_HEIGHT * 7 / 8))
+DROPDOWN_RECTANGLE=$(central_rectangle $DROPDOWN_WIDTH $DROPDOWN_HEIGHT)
+
+INFO_WIDTH=$(($SCREEN_WIDTH * 3 / 4))
+INFO_HEIGHT=$(($SCREEN_HEIGHT * 3 / 4))
+INFO_RECTANGLE=$(central_rectangle $INFO_WIDTH $INFO_HEIGHT)
+
+DICTIONARY_WIDTH=500
+DICTIONARY_HEIGHT=$(($SCREEN_HEIGHT * 19 / 20))
+DICTIONARY_RECTANGLE=$(central_rectangle $DICTIONARY_WIDTH $DICTIONARY_HEIGHT)
 
 #
 # Rules
 #
 
 case $CLASS_NAME in
-    dropdown_*)
-        echo "sticky=on state=floating hidden=on rectangle=$FLOATING_RECTANGLE"
-        ;;
+    dropdown_*) echo "sticky=on state=floating hidden=on rectangle=$DROPDOWN_RECTANGLE" ;;
 
-    info_*)
-        echo "sticky=on state=floating rectangle=$FLOATING_RECTANGLE"
-        ;;
+    info_dictionary) echo "state=floating rectangle=$DICTIONARY_RECTANGLE" ;;
+    info_*) echo "state=floating rectangle=$INFO_RECTANGLE" ;;
 
-    Surf|Sxiv|mpv|Zathura)
-        echo "state=fullscreen"
-        ;;
+    Sxiv|mpv|Zathura) echo "state=fullscreen" ;;
 esac
 
