5d278258813cafd63b7fecc9e8be8340dbd17ec1
ivanp7 <ivanp7@mail.ru>
2021-01-18 18:25:14 +0300

    Improve polybar configuration so it does not require auto-modification on installation anymore

--- 1-symlinked/tree/.scripts/xdf/autostart/async/status/polybar.sh
+++ 1-symlinked/tree/.scripts/xdf/autostart/async/status/polybar.sh
@@ -5,8 +5,8 @@ keep_polybar ()
     while xhost > /dev/null 2>&1
     do 
         [ "$1" = "$PRIMARY_MONITOR" ] &&
-            MONITOR=$1 TRAY_POS=right polybar --reload top ||
-            MONITOR=$1 polybar --reload top
+            MONITOR=$1 FONT="$DEFAULT_FONT;0" TRAY_POS=right polybar --reload top ||
+            MONITOR=$1 FONT="$DEFAULT_FONT;0" polybar --reload top
     done
 }
 
--- 9-special/instructions/polybar.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-[ -z "$DEFAULT_FONT" ] && DEFAULT_FONT="xos4 Terminus:size=10"
-
-sed -i 's/DEFAULT_FONT/'"$DEFAULT_FONT"'/g' $HOME/.config/polybar/config
-
