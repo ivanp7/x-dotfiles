77c99ee8cb949389fbe0ab8b9c5dc45aaaba3116
ivanp7 <ivanp7@mail.ru>
2021-01-26 17:32:35 +0300

    Improve volume-notify.sh

--- 1-symlinked/tree/bin/xdf/system-control/volume-notify.sh
+++ 1-symlinked/tree/bin/xdf/system-control/volume-notify.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 [ "$#" -gt 0 ] && volume.sh "$@"
-notify-send -t 1000 -u low "Volume: $(volume.sh)"
+VOLUME="$(volume.sh)"
+[ -n "$VOLUME" ] && notify-send -t 1000 -u low "$(volume.sh)" ||
+    notify-send -t 1000 -u low "No playback devices"
 
