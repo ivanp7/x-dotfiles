93f398772cb54eef08561c51a024a8a253f1cf07
ivanp7 <ivanp7@mail.ru>
2021-03-16 12:59:09 +0300

    Display shell info after exiting from X

--- 1-symlinked/tree/bin/xdf/x-desktop.sh
+++ 1-symlinked/tree/bin/xdf/x-desktop.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-cd $HOME
+# request display of shell info
+PIDFILE="$TMPDIR_CURRENT/shell_info$(tty)/$PARENT_SHELL_PID_EXPORTED"
+[ -n "$PARENT_SHELL_PID_EXPORTED" -a -f "$PIDFILE" ] && rm "$PIDFILE" || true
+
+cd
 exec startx "$XDG_CONFIG_HOME/X11/xinitrc" -- "$XDG_CONFIG_HOME/X11/xserverrc"
 
