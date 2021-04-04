52f7ec9f37e9f631f455491b1b5b7e8ddfba339c
ivanp7 <ivanp7@mail.ru>
2021-04-04 23:41:15 +0300

    Restore keymap after exiting from slock

--- 1-symlinked/tree/bin/xdf/graphical-environment/lock-screen.sh
+++ 1-symlinked/tree/bin/xdf/graphical-environment/lock-screen.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
+lock_screen ()
+{
+    # switch keyboard to the default state
+    kbd-language.sh english
+    kbd-layout.sh qwerty
+
+    # lock
+    slock
+
+    # restore keymap
+    xmodmap "$XDG_CONFIG_HOME/X11/xmodmap"
+}
+
 STATUS_FILE="/tmp/no-screen-lock_$USER"
 case "$1" in
     -on) rm $STATUS_FILE ;;
     -off) touch $STATUS_FILE ;;
     -*) [ -f "$STATUS_FILE" ] && echo disabled || echo enabled ;;
-    *) [ -f "$STATUS_FILE" ] || { kbd-language.sh english; kbd-layout.sh qwerty; slock; } & $@
+    *) [ -f "$STATUS_FILE" ] || lock_screen & $@
 esac
 
