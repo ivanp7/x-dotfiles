5d94728d23307dfb7bac21d74e82a5ad213407e5
ivanp7 <ivanp7@mail.ru>
2021-05-22 18:48:11 +0300

    Fix tmux-clipboard.sh for multiline data

--- 1-symlinked/tree/bin/xdf/applications/tmux-clipboard.sh
+++ 1-symlinked/tree/bin/xdf/applications/tmux-clipboard.sh
@@ -7,6 +7,6 @@ DMENU_COLUMNS=2
 BUFFER="$(tmux list-buffers | dmenu.sh -p "$DMENU_PROMPT" -l $DMENU_LINES -g $DMENU_COLUMNS | head -1)"
 [ -z "$BUFFER" ] && exit
 
-BUFFER="$(echo "$BUFFER" | cut -d':' -f 1)"
+BUFFER="$(echo "$BUFFER" | head -1 | cut -d':' -f 1)"
 tmux save-buffer -b "$BUFFER" - | xclip -sel clip
 
