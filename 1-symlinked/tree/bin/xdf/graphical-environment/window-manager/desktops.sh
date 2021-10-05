6c78928d1151a09492a50099f64ef8b4ba2eb126
ivanp7 <ivanp7@mail.ru>
2021-10-06 02:49:12 +0300

    Allow desktops to be renamed using environment variables

--- 1-symlinked/tree/bin/xdf/graphical-environment/window-manager/desktops.sh
+++ 1-symlinked/tree/bin/xdf/graphical-environment/window-manager/desktops.sh
@@ -4,9 +4,20 @@ MONITOR=$2
 
 N=$(bspc query -D -m $MONITOR | wc -l)
 
+: ${DESKTOP_1:="I"}
+: ${DESKTOP_2:="II"}
+: ${DESKTOP_3:="III"}
+: ${DESKTOP_4:="IV"}
+: ${DESKTOP_5:="V"}
+: ${DESKTOP_6:="VI"}
+: ${DESKTOP_7:="VII"}
+: ${DESKTOP_8:="VIII"}
+: ${DESKTOP_9:="IX"}
+: ${DESKTOP_10:="X"}
+
 to_name ()
 {
-    sed 's/10/X/;s/1/I/;s/2/II/;s/3/III/;s/4/IV/;s/5/V/;s/6/VI/;s/7/VII/;s/8/VIII/;s/9/IX/'
+    sed "s/10/${DESKTOP_10}/;s/1/${DESKTOP_1}/;s/2/${DESKTOP_2}/;s/3/${DESKTOP_3}/;s/4/${DESKTOP_4}/;s/5/${DESKTOP_5}/;s/6/${DESKTOP_6}/;s/7/${DESKTOP_7}/;s/8/${DESKTOP_8}/;s/9/${DESKTOP_9}/"
 }
 
 case $1 in
